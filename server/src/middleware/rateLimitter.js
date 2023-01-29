53337ccb08dee4dd262451973bcd7529ae30778f
Valeriy Tetevin <seal.matob@gmail.com>
2023-01-30 00:02:54 +0300

    Add RefreshToken method.  add small refactoring Fixes #3

--- server/src/middleware/rateLimitter.js
+++ server/src/middleware/rateLimitter.js
@@ -1,34 +1,28 @@
 'use strict';
 
-import dotenv from 'dotenv'
-dotenv.config();
-
 import redis from 'redis';
 import { RateLimiterRedis } from 'rate-limiter-flexible';
 
-const url = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
-const redisClient = redis.createClient({
-    url: url,
-});
+/**
+ * 
+ * @param {redis} redisClient 
+ */
+export function newRateLimmiterMiddleware(redisClient) {
+    const rateLimiter = new RateLimiterRedis({
+        storeClient: redisClient,
+        keyPrefix: 'middleware',
+        points: 10, // 10 requests
+        duration: 1, // per 1 second by IP
+    });
 
-redisClient.on('error', err => console.log('Redis Client Error', err));
-await redisClient.connect();
-await redisClient.PING();
-
-const rateLimiter = new RateLimiterRedis({
-    storeClient: redisClient,
-    keyPrefix: 'middleware',
-    points: 10, // 10 requests
-    duration: 1, // per 1 second by IP
-});
-
-export function rateLimiterMiddleware(req, res, next) {
-    console.log(`req.ip ${req.ip}`);
-    rateLimiter.consume(req.ip, 2)
-        .then(() => {
-            next();
-        })
-        .catch(() => {
-            res.status(429).send('Too Many Requests');
-        });
-};
+    return function (req, res, next) {
+        console.log(`req.ip ${req.ip}`);
+        rateLimiter.consume(req.ip, 2)
+            .then(() => {
+                next();
+            })
+            .catch(() => {
+                res.status(429).send('Too Many Requests');
+            });
+    }
+}
--- server/src/repository/repo.js
+++ server/src/repository/repo.js
@@ -2,3 +2,4 @@
 
 export { UserRepo } from "./userrepo.js";
 export { TodosRepo } from "./todosrepo.js";
+export { TokensRepo } from "./tokensrepo.js";
